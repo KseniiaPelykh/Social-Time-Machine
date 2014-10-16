0b09cc55a71515cfc3f79335f38b4ac80d6363ba
Kseniia <kseniia.pelykh@gmail.com>
2014-10-16 21:28:22 +0200

    added new game activity

--- src/com/example/socialtimemachine/HomeActivity.java
+++ src/com/example/socialtimemachine/HomeActivity.java
@@ -1,13 +1,20 @@
 package com.example.socialtimemachine;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+	}
+	
+	public void newGame(View view){
+		Intent intent = new Intent(this, NewGameActivity.class);
+		startActivity(intent);
 	}
 	
 }
---
+++ src/com/example/socialtimemachine/NewGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.socialtimemachine;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewGameActivity extends Activity {
+	
+	protected void onCreate(Bundle savedInstanceState){
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_newgame);
+	}
+	
+}
