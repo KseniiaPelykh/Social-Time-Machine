42490afb9bd102cca89c7876032f3892ed174e3e
Kseniia <kseniia.pelykh@gmail.com>
2014-10-25 18:49:34 +0200

    image upload on create new button onclick event

--- src/com/example/socialtimemachine/NewGameActivity.java
+++ src/com/example/socialtimemachine/NewGameActivity.java
@@ -1,13 +1,87 @@
 package com.example.socialtimemachine;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.parse.Parse;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
 
 public class NewGameActivity extends Activity {
 	
+	public static final int RESULT_LOAD_IMAGE = 1;
+	
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		
+		Parse.initialize(this, "CblPQNXB5bztS0zjzox1vPPb8mRCiOorvNQMD3Jb", "fcqLiSWLa2JVHMW0esKZP3ewkAJm0jYPEjhlYVmg");
 		setContentView(R.layout.activity_newgame);
+		
+		Button buttonLoadImage = (Button) findViewById(R.id.buttonLoadPicture);
+		buttonLoadImage.setOnClickListener(new View.OnClickListener()
+		{
+			@Override
+			public void onClick(View arg0){
+				Intent intent = new Intent(Intent.ACTION_PICK);
+				intent.setType("image/*");
+				startActivityForResult(intent, RESULT_LOAD_IMAGE);
+			}
+		});		
 	}
 	
+	public void saveGame(View view){
+		EditText titleOfGame = (EditText)findViewById(R.id.title_of_game);
+		EditText textOfUser = (EditText)findViewById(R.id.text_of_user);
+		
+		ParseObject newgame = new ParseObject("Game");
+		newgame.put("gameTitle", titleOfGame.getText().toString());
+		newgame.put("gameText", textOfUser.getText().toString());
+		
+		ImageView gameImage = (ImageView)findViewById(R.id.imgView);
+		Bitmap bitmap = ((BitmapDrawable) gameImage.getDrawable()).getBitmap();
+	    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+	    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+		ParseFile file = new ParseFile("picturePath.png", stream.toByteArray());
+		newgame.put("gameImage", file);
+		newgame.saveInBackground();
+		
+		Intent intent = new Intent(this, HomeActivity.class);
+		startActivity(intent);		
+	}
+		
+	@Override 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data){
+			Uri selectedImage = data.getData();
+			String[] filePathColumn = { MediaStore.Images.Media.DATA };
+		
+			Cursor cursor = getContentResolver().query(selectedImage,
+					filePathColumn, null, null, null);
+			int columnIndex = cursor.getColumnIndex(MediaStore.Images.Media.DATA);
+			cursor.moveToFirst();
+			String picturePath = cursor.getString(columnIndex);
+			
+			File image = new File(picturePath);
+			Bitmap bitmap = BitmapFactory.decodeFile(image.getAbsolutePath());			
+			ImageView gameImage = (ImageView) findViewById(R.id.imgView);
+			gameImage.setImageBitmap(bitmap);			
+		}
+	}
 }
